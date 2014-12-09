344bc71ea324cc8bdce258e1c08dc577c1c5635b
Postmodern <postmodern.mod3@gmail.com>
2014-12-09 14:33:48 -0800

    Added a spec for Parser.parse.

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -573,4 +573,19 @@ describe Parser do
       end
     end
   end
+
+  describe Parser::Transform do
+  end
+
+  describe ".parse" do
+    let(:spf) do
+      %{v=spf1 ip4:199.16.156.0/22 ip4:199.59.148.0/22 ip4:8.25.194.0/23 ip4:8.25.196.0/23 ip4:204.92.114.203 ip4:204.92.114.204/31 ip4:107.20.52.15 ip4:23.21.83.90 include:_spf.google.com include:_thirdparty.twitter.com all}
+    end
+
+    subject { described_class.parse(spf) }
+
+    it "should return a Record" do
+      expect(subject).to be_kind_of(Record)
+    end
+  end
 end
