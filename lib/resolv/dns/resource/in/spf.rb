899bc0756b67b3eb0839332da73a8c1e7593838f
Postmodern <postmodern.mod3@gmail.com>
2015-06-26 11:28:31 -0700

    We have to register new resource classes with ClassHash.

--- lib/resolv/dns/resource/in/spf.rb
+++ lib/resolv/dns/resource/in/spf.rb
@@ -3,4 +3,7 @@ require 'resolv'
 class Resolv::DNS::Resource::IN::SPF < Resolv::DNS::Resource::IN::TXT
   # resolv.rb doesn't define an SPF resource type.
   TypeValue = 99
+  ClassValue = Resolv::DNS::Resource::IN::ClassValue
+
+  Resolv::DNS::Resource::ClassHash[[TypeValue, ClassValue]] = self
 end
